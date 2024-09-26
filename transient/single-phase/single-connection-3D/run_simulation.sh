aa9f59343e3d285a15a36fbedb18ac30fe595212
jvela018 <jonvelasco00@gmail.com>
2024-09-26 22:17:03 +0300

    add more complex cases

---
+++ transient/single-phase/single-connection-3D/run_simulation.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+# remove previous simulation results
+rm -rf results_*
+
+# run Elmer's simulation
+#ElmerSolver current_massive.sif
+#ElmerSolver current_stranded.sif
+
+#ElmerSolver voltage_massive.sif
+ElmerSolver voltage_stranded.sif
+
+# plot scalar results
+python3 plot_results_data.py
